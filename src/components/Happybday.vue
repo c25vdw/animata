c225dfa14a2ecc57f72c02a5c3b1c787790f3ce2
Lucas Zeng <lucaszeng99@gmail.com>
2018-04-21 21:16:31 -0600

    balloon is floating up

--- src/components/Happybday.vue
+++ src/components/Happybday.vue
@@ -37,10 +37,16 @@ export default {
   props: ["skipped"],
 }
 </script>
-<style lang="less">
-@total-time: 4s;
+<style lang="less" scoped>
+// timing
+@total-time: 8s;
 @half-time: calc(@total-time/2);
 @transition-time: 1s;
+
+* {
+  overflow: hidden;
+  margin: 0; padding: 0;
+}
 #stage-balloon {
   width: 100%; height: 100%;
   background-color: antiquewhite;
@@ -51,11 +57,41 @@ export default {
   animation: popup @transition-time ease-in-out;
 }
 
-#stage-balloon balloon {
-  width: 20%; height: 20%;
+#stage-balloon .balloon {
+  position: absolute;
+  top: 100%;
+  text-align: center;
+  .text {
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  &.balloon-1 { left: 3%; }
+  &.balloon-2 { left: 14%; }
+  &.balloon-3 { left: 22%; }
+  &.balloon-4 { left: 37%; }
+  &.balloon-5 { left: 50%; }
+  &.balloon-6 { left: 56%; }
+  &.balloon-7 { left: 75%; }
+  &.balloon-8 { left: 84%; }
+  width: 80px; height: 100px;
   background-color: burlywood;
-  
+  border-radius: 50%;
+  ::after {
+    position: absolute;
+    bottom: 50%; left: 50%;
+    width: 2px; height: 20px;
+    border: 4px solid blue;
+  }
 }
+#stage-balloon .balloon-1 { animation: floatup 4s ease 1s forwards; }
+#stage-balloon .balloon-2 { animation: floatup 4s ease 1.15s forwards; }
+#stage-balloon .balloon-3 { animation: floatup 4s ease 1.3s forwards; }
+#stage-balloon .balloon-4 { animation: floatup 4s ease 1.31s forwards; }
+#stage-balloon .balloon-5 { animation: floatup 4s ease 1.6s forwards; }
+#stage-balloon .balloon-6 { animation: floatup 4s ease 1.7s forwards; }
+#stage-balloon .balloon-7 { animation: floatup 4s ease 1.75s forwards; }
+#stage-balloon .balloon-8 { animation: floatup 4s ease 2.1s forwards; }
 
 #stage-popme {
   width: 100%; height: 100%;
@@ -73,5 +109,9 @@ export default {
 @keyframes popup {
   0%   { opacity: 0; }
   100% { opacity: 1; }
+}
+@keyframes floatup {
+  0%   { top: 100%; }
+  100% { top: -40%; }
 }
 </style>
