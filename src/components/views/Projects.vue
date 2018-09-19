37f0b6c7ac1d08bcc0aea37a95858cc80779c071
Lucas Zeng <lucaszeng99@gmail.com>
2018-09-18 22:06:42 -0600

    deprecating version 1

--- src/components/views/Projects.vue
+++ src/components/views/Projects.vue
@@ -3,15 +3,21 @@
       <div class="title"><h1>Projects</h1></div>
       <div class="content">
           <ul class="project-list">
-              <li>
+                <li>
                   <a href="http://calculator.lucaszzc.com" target="_blank" class="project-link">Calculator</a>
                   <a href="https://github.com/LucasZeng99/vue-simple-calculator" target="_blank" class="github-link"><img src="/static/GitHub-Mark-64px.png"></img></a>
                   </li>
-              <li>
+                <li>
                   <a href="http://chatroom.lucaszzc.com" target="_blank" class="project-link">Chatroom</a>
                   <a href="https://github.com/LucasZeng99/chatroom" target="_blank" class="github-link"><img src="/static/GitHub-Mark-64px.png"></img></a>
                   </li>
-              <li>
+                <li>
+                  <a href="https://github.com/LucasZeng99/blug" target="_blank" class="github-link">Blug -blog generator<img src="/static/GitHub-Mark-64px.png"></img></a>
+                  </li>
+                <li>
+                  <a href="https://github.com/LucasZeng99/vanilla-todo" target="_blank" class="github-link">Todo-list -desktop todo app<img src="/static/GitHub-Mark-64px.png"></img></a>
+                  </li>
+                <li>
                   <a href="http://weather.lucaszzc.com" target="_blank" class="project-link">Weather Panel</a>
                   <a href="https://github.com/LucasZeng99/vue-weather-panel" target="_blank" class="github-link"><img src="/static/GitHub-Mark-64px.png"></img></a>
                   </li>
